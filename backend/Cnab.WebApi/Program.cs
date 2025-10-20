abbe379e396072f08f2d5d26979522fb5da958a8
Ricardo Kramer <rvkramer@gmail.com>
2025-10-20 12:33:14 -0600

    Configure Swagger for API documentation

--- backend/Cnab.WebApi/Program.cs
+++ backend/Cnab.WebApi/Program.cs
@@ -18,19 +18,23 @@ builder.Services.AddScoped<IStoreRepository, StoreRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
